4cf4fb4fd98b6eee5281382ec611c918ba33f0da
Roma Volkov <Drakyla60@gmail.com>
2021-09-27 23:24:01 +0300

    Фабрика RegistrationControllerFactory

---
+++ module/Application/src/Controller/Factory/RegistrationControllerFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Controller\Factory;
+
+use Application\Controller\RegistrationController;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+
+class RegistrationControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $sessionContainer = $container->get('UserRegistration');
+
+        // Инстанцируем контроллер и внедряем зависимости.
+        return new RegistrationController($sessionContainer);
+    }
+}
