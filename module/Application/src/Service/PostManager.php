ddfb3f4eda0d8b3f44b0cfc740e3c1e13eb0ee38
Roma Volkov <Drakyla60@gmail.com>
2021-10-13 16:36:26 +0300

    Сервіс PostManager  для нового поста

---
+++ module/Application/src/Service/PostManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Application\Service;
+
+use Application\Entity\Post;
+use Application\Entity\Tag;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Laminas\Filter\StaticFilter;
+use Laminas\Filter\StringTrim;
+
+class PostManager
+{
+    private EntityManager $entityManager;
+
+    public function __construct($entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function addNewPost($data)
+    {
+        $post = new Post();
+        $post->setTitle($data['title']);
+        $post->setContent($data['content']);
+        $post->setStatus($data['status']);
+        $post->setDateCreated(date('Y-m-d H:i:s'));
+
+        $this->entityManager->persist($post);
+
+        $this->addTagsToPost($data['tags'], $post);
+        
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @throws ORMException
+     */
+    private function addTagsToPost(string $tagsStr, Post $post)
+    {
+        $tags = $post->getTags();
+        foreach ($tags as $tag) {
+            $post->removeTagAssociation($tag);
+        }
+
+        $tags = explode(',', $tagsStr);
+        foreach ($tags as $tagName){
+            $tagName = StaticFilter::execute($tagName, StringTrim::class);
+
+            if (empty($tagName)) continue;
+
+            $tag = $this
+                ->entityManager
+                ->getRepository(Tag::class)
+                ->findOneByName($tagName);
+
+            if (null == $tag)
+                $tag = new Tag();
+
+            $tag->setName($tagName);
+            $tag->addPost($post);
+
+            $this->entityManager->persist($tag);
+
+            $post->addTag($tag);
+        }
+    }
+}
