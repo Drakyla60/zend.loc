73d673b04ea72c4aa3467edcbbbde82a31edc105
Roma Volkov <Drakyla60@gmail.com>
2021-10-17 15:20:40 +0300

    Через обробник подій переоприділяємо головний layout

--- module/Admin/src/Module.php
+++ module/Admin/src/Module.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Admin;
 
+use Laminas\ModuleManager\ModuleManager;
+use Laminas\Mvc\MvcEvent;
+
 class Module
 {
     public function getConfig(): array
@@ -12,4 +15,30 @@ class Module
         $config = include __DIR__ . '/../config/module.config.php';
         return $config;
     }
+    // Метод "init" викликається при запуску програми і
+    // дозволяє зареєструвати обробник подій.
+
+    public function init(ModuleManager $manager)
+    {
+        $eventManager = $manager->getEventManager();
+        $sharedEventManager = $eventManager->getSharedManager();
+
+        $sharedEventManager->attach(__NAMESPACE__, 'dispatch',
+            [$this, 'onDispatch'], 100);
+
+    }
+
+    // Через обробник подій переоприділяємо головний layout
+    public function onDispatch(MvcEvent $event)
+    {
+        $controller = $event->getTarget();
+        $controllerClass = get_class($controller);
+        $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+
+        if ($moduleNamespace == __NAMESPACE__) {
+            $viewModel = $event->getViewModel();
+            $viewModel->setTemplate('layout/admin_layout');
+        }
+    }
+
 }
