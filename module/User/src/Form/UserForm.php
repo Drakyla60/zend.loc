85846daca0cefeb3cec385d882bd5e9e16785062
Roma Volkov <Drakyla60@gmail.com>
2021-10-22 14:29:13 +0300

    Форма для створення і обновлення користувачів, приділяється по $this->scenario

---
+++ module/User/src/Form/UserForm.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace User\Form;
+
+use Laminas\Filter\ToInt;
+use Laminas\Form\Form;
+use Laminas\InputFilter\ArrayInput;
+use Laminas\Validator\Hostname;
+
+class UserForm extends Form
+{
+    private $scenario;
+
+    public function __construct($scenario = 'create')
+    {
+        parent::__construct('user-form');
+        $this->setAttribute('method', 'post');
+        $this->scenario = $scenario;
+
+        $this->addElements();
+        $this->addInputFilter();
+    }
+
+    /**
+     * This method adds elements to form (input fields and submit button).
+     */
+    protected function addElements()
+    {
+        $this->add([
+            'type' => 'text',
+            'name' => 'email',
+            'options' => [
+                'label' => 'E-mail',
+            ],
+        ]);
+
+        $this->add([
+            'type' => 'text',
+            'name' => 'full_name',
+            'options' => [
+                'label' => 'Full Name',
+            ],
+        ]);
+
+        if ($this->scenario == 'create') {
+
+            $this->add([
+                'type' => 'password',
+                'name' => 'password',
+                'options' => [
+                    'label' => 'Password',
+                ],
+            ]);
+
+            $this->add([
+                'type' => 'password',
+                'name' => 'confirm_password',
+                'options' => [
+                    'label' => 'Confirm password',
+                ],
+            ]);
+        }
+
+        $this->add([
+            'type' => 'select',
+            'name' => 'status',
+            'options' => [
+                'label' => 'Status',
+                'value_options' => [
+                    1 => 'Active',
+                    2 => 'Retired',
+                ]
+            ],
+        ]);
+
+        $this->add([
+            'type' => 'csrf',
+            'name' => 'csrf',
+            'options' => [
+                'csrf_options' => [
+                    'timeout' => 600
+                ]
+            ],
+        ]);
+
+        $this->add([
+            'type' => 'submit',
+            'name' => 'submit',
+            'attributes' => [
+                'value' => 'Create'
+            ],
+        ]);
+    }
+
+    /**
+     * This method creates input filter (used for form filtering/validation).
+     */
+    private function addInputFilter()
+    {
+        $inputFilter = $this->getInputFilter();
+
+        $inputFilter->add([
+            'name' => 'email',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 128
+                    ],
+                ],
+                [
+                    'name' => 'EmailAddress',
+                    'options' => [
+                        'allow' => Hostname::ALLOW_DNS,
+                        'useMxCheck' => false,
+                    ],
+                ],
+            ],
+        ]);
+
+        $inputFilter->add([
+            'name' => 'full_name',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 512
+                    ],
+                ],
+            ],
+        ]);
+
+        if ($this->scenario == 'create') {
+
+            $inputFilter->add([
+                'name' => 'password',
+                'required' => true,
+                'filters' => [
+                ],
+                'validators' => [
+                    [
+                        'name' => 'StringLength',
+                        'options' => [
+                            'min' => 6,
+                            'max' => 64
+                        ],
+                    ],
+                ],
+            ]);
+
+            $inputFilter->add([
+                'name' => 'confirm_password',
+                'required' => true,
+                'filters' => [
+                ],
+                'validators' => [
+                    [
+                        'name' => 'Identical',
+                        'options' => [
+                            'token' => 'password',
+                        ],
+                    ],
+                ],
+            ]);
+        }
+
+        $inputFilter->add([
+            'name' => 'status',
+            'required' => true,
+            'filters' => [
+                ['name' => ToInt::class],
+            ],
+            'validators' => [
+                [
+                    'name' => 'InArray',
+                    'options' => [
+                        'haystack' => [1, 2]
+                    ]
+                ]
+            ],
+        ]);
+    }
+}
