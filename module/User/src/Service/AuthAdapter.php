77ced41ed8c1a0b27be827409b085fd40d1fe406
Roma Volkov <Drakyla60@gmail.com>
2021-10-23 14:56:32 +0300

    Сервіс для аутентифікації користувача, реалізує AdapterInterface

--- module/User/src/Service/AuthAdapter.php
+++ module/User/src/Service/AuthAdapter.php
@@ -2,62 +2,45 @@
 
 namespace User\Service;
 
+use Laminas\Authentication\Adapter\AdapterInterface;
 use Laminas\Authentication\Result;
 use Laminas\Crypt\Password\Bcrypt;
 use User\Entity\User;
 
-class AuthAdapter
+class AuthAdapter implements AdapterInterface
 {
-
     private $email;
-
     private $password;
-
     private $entityManager;
-
 
     public function __construct($entityManager)
     {
         $this->entityManager = $entityManager;
     }
 
-    /**
-     * Задает эл. адрес пользователя.
-     */
-    public function setEmail($email)
+    public function setEmail(string $email)
     {
         $this->email = $email;
     }
 
-    /**
-     * Устанавливает пароль.
-     */
-    public function setPassword($password)
+    public function setPassword(string $password)
     {
-        $this->password = (string)$password;
+        $this->password = $password;
     }
 
     public function authenticate()
     {
-        // Проверяем, есть ли в базе данных пользователь с таким адресом.
         $user = $this->entityManager->getRepository(User::class)
             ->findOneByEmail($this->email);
 
-        // Если такого пользователя нет, возвращаем статус 'Identity Not Found'.
         if ($user == null) {
-            return new Result(
-                Result::FAILURE_IDENTITY_NOT_FOUND,
-                null,
-                ['Invalid credentials.']);
+            return new Result(Result::FAILURE_IDENTITY_NOT_FOUND, null, ['Invalid credentials.']);
         }
 
-        // Если пользователь с таким адресом существует, необходимо проверить, активен ли он.
-        // Неактивные пользователи не могут входить в систему.
+        // Якщо користувач із такою адресою існує, необхідно перевірити, чи активний він.
+        // Неактивні користувачі не можуть входити в систему.
         if ($user->getStatus()==User::STATUS_RETIRED) {
-            return new Result(
-                Result::FAILURE,
-                null,
-                ['User is retired.']);
+            return new Result(Result::FAILURE, null, ['User is retired.']);
         }
 
         // Теперь необходимо вычислить хэш на основе введенного пользователем пароля и сравнить его
@@ -66,18 +49,12 @@ class AuthAdapter
         $passwordHash = $user->getPassword();
 
         if ($bcrypt->verify($this->password, $passwordHash)) {
-            // Отлично! Хэши паролей совпадают. Возвращаем личность пользователя (эл. адрес) для
-            // хранения в сессии с целью последующего использования.
-            return new Result(
-                Result::SUCCESS,
-                $this->email,
-                ['Authenticated successfully.']);
+            // Хеши паролів збігаються. Повертаємо особистість користувача (ел. Адреса) для
+            // зберігання в сесії з метою подальшого використання.
+            return new Result(Result::SUCCESS, $this->email, ['Authenticated successfully.']);
         }
 
-        // Если пароль не прошел проверку, возвращаем статус ошибки 'Invalid Credential'.
-        return new Result(
-            Result::FAILURE_CREDENTIAL_INVALID,
-            null,
-            ['Invalid credentials.']);
+        // Якщо пароль не пройшов перевірку, повертаємо статус помилки 'Invalid Credential'.
+        return new Result(Result::FAILURE_CREDENTIAL_INVALID, null, ['Invalid credentials.']);
     }
 }
