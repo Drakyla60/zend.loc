db38e117641aa712c6841c17f248a64de404223b
Roma Volkov <Drakyla60@gmail.com>
2021-10-23 21:06:58 +0300

    Додав правила для доступу

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace User;
 
+use Application\Controller\IndexController;
 use Laminas\Authentication\AuthenticationService;
 use Laminas\Router\Http\Segment;
-use Laminas\ServiceManager\Factory\InvokableFactory;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Laminas\Router\Http\Literal;
 use User\Controller\Factory\AuthControllerFactory;
@@ -19,7 +19,7 @@ use User\Service\AuthManager;
 use User\Service\UserManager;
 
 return [
-    'router' => [
+    'router'             => [
         'routes' => [
             'home_user' => [
                 'type'    => Literal::class,
@@ -87,33 +87,33 @@ return [
             ],
         ],
     ],
-    'controllers' => [
+    'controllers'        => [
         'factories' => [
             Controller\UserController::class => Controller\Factory\UserControllerFactory::class,
             Controller\AuthController::class => AuthControllerFactory::class,
         ],
     ],
-    'service_manager' => [
+    'service_manager'    => [
         'factories' => [
             AuthenticationService::class => AuthenticationServiceFactory::class,
-            AuthAdapter::class => AuthAdapterFactory::class,
-            UserManager::class => UserManagerFactory::class,
-            AuthManager::class => AuthManagerFactory::class,
+            AuthAdapter::class           => AuthAdapterFactory::class,
+            UserManager::class           => UserManagerFactory::class,
+            AuthManager::class           => AuthManagerFactory::class,
         ],
     ],
-    'view_manager' => [
+    'view_manager'       => [
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
-        'template_map' => [
+        'template_map'        => [
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ],
-        'strategies' => [
+        'strategies'          => [
             'ViewJsonStrategy',
         ],
         'template_path_stack' => [
@@ -123,8 +123,7 @@ return [
     'session_containers' => [
         'UserSessionContainer',
     ],
-
-    'doctrine' => [
+    'doctrine'           => [
         'driver' => [
             __NAMESPACE__ . '_driver' => [
                 'class' => AnnotationDriver::class,
@@ -138,25 +137,19 @@ return [
             ]
         ]
     ],
-//    // Ключ 'access_filter' используется модулем User, чтобы разрешить или запретить доступ к
-//// определенным действиям контроллера для не вошедших на сайт пользователей.
-//    'access_filter' => [
-//        'options' => [
-//            // Фильтр доступа может работать в 'ограничительном' (рекомендуется) или 'разрешающем'
-//            // режиме. В ограничительном режиме все действия контроллера должны быть явно перечислены
-//            // под ключом конфигурации 'access_filter', а доступ к любому не перечисленному действию
-//            // для неавторизованных пользователей запрещен. В разрешающем режиме, даже если действие не
-//            // указано под ключом 'access_filter', доступ к нему разрешен для всех (даже для
-//            // неавторизованных пользователей. Рекомендуется использовать более безопасный ограничительный режим.
-//            'mode' => 'restrictive'
-//        ],
-//        'controllers' => [
-//            Controller\UserController::class => [
-//                // Позволяем всем обращаться к действиям "index" и "about".
-//                ['actions' => ['index', 'about'], 'allow' => '*'],
-//                // Позволяем вошедшим на сайт пользователям обращаться к действию "settings".
-//                ['actions' => ['settings'], 'allow' => '@']
-//            ],
-//        ]
-//    ],
+    'access_filter'      => [
+        'options' => [
+            'mode' => 'restrictive' // restrictive  !!  permissive
+        ],
+        'controllers' => [
+            Controller\UserController::class => [
+                ['actions' => ['index'], 'allow' => '*'],
+                ['actions' => ['add'], 'allow' => '@']
+            ],
+            IndexController::class => [
+                ['actions' => ['index'], 'allow' => '*'],
+                ['actions' => ['about'], 'allow' => '@']
+            ],
+        ]
+    ],
 ];
