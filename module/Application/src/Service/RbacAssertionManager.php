954384c86e1eed4e99aff3c02bcc0453a2d229f7
Roma Volkov <Drakyla60@gmail.com>
2021-10-25 16:20:06 +0300

    RbacAssertionManager для перевірки доступу

---
+++ module/Application/src/Service/RbacAssertionManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Application\Service;
+
+use Doctrine\ORM\EntityManager;
+use Laminas\Authentication\AuthenticationService;
+use Laminas\Permissions\Rbac\Rbac;
+use User\Entity\User;
+
+class RbacAssertionManager
+{
+    private EntityManager $entityManager;
+    private AuthenticationService $authService;
+
+    public function __construct($entityManager, $authService)
+    {
+        $this->entityManager = $entityManager;
+        $this->authService = $authService;
+    }
+
+    public function assert(Rbac $rbac, $permission, $params): bool
+    {
+        $currentUser = $this
+            ->entityManager
+            ->getRepository(User::class)
+            ->findOneByEmail($this->authService->getIdentity());
+
+        if ($permission == 'profile.own.view' && $params['user']->getId() == $currentUser->getId()) {
+            return true;
+        }
+        return false;
+    }
+}
