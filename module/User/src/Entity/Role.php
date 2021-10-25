f5062f2162ad13f16368db1f8448b0734f844798
Roma Volkov <Drakyla60@gmail.com>
2021-10-25 16:18:40 +0300

    Сутність для роботи з ролями

---
+++ module/User/src/Entity/Role.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace User\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Цей клас представляє Роль для користувача.
+ * @ORM\Entity()
+ * @ORM\Table(name="role")
+ */
+class Role
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="name")
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(name="description")
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(name="date_created")
+     */
+    protected $dateCreated;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="User\Entity\Role")
+     * @ORM\JoinTable(name="role_hierarchy",
+     *      joinColumns={@ORM\JoinColumn(name="child_role_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="parent_role_id", referencedColumnName="id")}
+     *      )
+     */
+    private $parentRoles;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="User\Entity\Role")
+     * @ORM\JoinTable(name="role_hierarchy",
+     *      joinColumns={@ORM\JoinColumn(name="parent_role_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="child_role_id", referencedColumnName="id")}
+     *      )
+     */
+    protected $childRoles;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="User\Entity\Permission")
+     * @ORM\JoinTable(name="role_permission",
+     *      joinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="permission_id", referencedColumnName="id")}
+     *      )
+     */
+    private $permissions;
+
+    public function __construct()
+    {
+        $this->parentRoles = new ArrayCollection();
+        $this->childRoles = new ArrayCollection();
+        $this->permissions = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+    }
+
+    public function getParentRoles()
+    {
+        return $this->parentRoles;
+    }
+
+    public function getChildRoles()
+    {
+        return $this->childRoles;
+    }
+
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    public function addParent(Role $role)
+    {
+        if ($this->getId() == $role->getId()) {
+            return false;
+        }
+        if (!$this->hasParent($role)) {
+            $this->parentRoles[] = $role;
+            return true;
+        }
+        return false;
+    }
+
+    public function clearParentRoles()
+    {
+        $this->parentRoles = new ArrayCollection();
+    }
+
+    public function hasParent(Role $role)
+    {
+        if ($this->getParentRoles()->contains($role)) {
+            return true;
+        }
+        return false;
+    }
+}
