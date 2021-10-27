bdae3496eb4a541fc26e0b2594fb963febddec0c
Roma Volkov <Drakyla60@gmail.com>
2021-10-27 21:50:24 +0300

    В фабрику AuthManagerFactory прокинув сервіс RbacManager

--- module/User/src/Service/Factory/AuthManagerFactory.php
+++ module/User/src/Service/Factory/AuthManagerFactory.php
@@ -7,6 +7,7 @@ use Laminas\Authentication\AuthenticationService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Laminas\Session\SessionManager;
 use User\Service\AuthManager;
+use User\Service\RbacManager;
 
 class AuthManagerFactory implements FactoryInterface
 {
@@ -15,6 +16,7 @@ class AuthManagerFactory implements FactoryInterface
     {
         $authService = $container->get(AuthenticationService::class);
         $sessionManager = $container->get(SessionManager::class);
+        $rbacManager = $container->get(RbacManager::class);
 
         // Get contents of 'access_filter' config key (the AuthManager service
         // will use this data to determine whether to allow currently logged in user
@@ -22,7 +24,7 @@ class AuthManagerFactory implements FactoryInterface
         $config = $container->get('Config');
         $config = $config['access_filter'] ?? [];
 
-        return new AuthManager($authService, $sessionManager, $config);
+        return new AuthManager($authService, $sessionManager, $config, $rbacManager);
 
     }
 }
