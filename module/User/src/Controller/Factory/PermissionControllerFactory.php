5a18392bf4859b33414878f8fd18dd20970c4e10
Roma Volkov <Drakyla60@gmail.com>
2021-10-27 21:32:50 +0300

    Фабрика PermissionControllerFactory для PermissionController

---
+++ module/User/src/Controller/Factory/PermissionControllerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace User\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use User\Controller\PermissionController;
+use User\Service\PermissionManager;
+
+class PermissionControllerFactory implements FactoryInterface
+{
+
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): PermissionController
+    {
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        $permissionManager = $container->get(PermissionManager::class);
+
+        // Instantiate the controller and inject dependencies
+        return new PermissionController($entityManager, $permissionManager);
+    }
+}
