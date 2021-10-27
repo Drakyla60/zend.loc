baadec3a570184a067ac88d7f767cbd19fbf1e21
Roma Volkov <Drakyla60@gmail.com>
2021-10-27 22:09:43 +0300

    Додав в UserManagerFactory потрібні залежності

--- module/User/src/Service/Factory/UserManagerFactory.php
+++ module/User/src/Service/Factory/UserManagerFactory.php
@@ -5,6 +5,8 @@ namespace User\Service\Factory;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Laminas\View\Model\ViewModel;
 use Psr\Container\ContainerInterface;
+use User\Service\PermissionManager;
+use User\Service\RoleManager;
 use User\Service\UserManager;
 
 class UserManagerFactory implements FactoryInterface
@@ -12,8 +14,11 @@ class UserManagerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): UserManager
     {
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        $roleManager = $container->get(RoleManager::class);
+        $permissionManager = $container->get(PermissionManager::class);
         $viewRenderer = $container->get('ViewRenderer');
+        $config = $container->get('Config');
 
-        return new UserManager($entityManager, $viewRenderer);
+        return new UserManager($entityManager, $roleManager, $permissionManager, $viewRenderer, $config);
     }
 }
