f2949bc169009e409ff4cb4ff44df0306e04118e
Roma Volkov <Drakyla60@gmail.com>
2021-10-28 23:18:44 +0300

    NavManagerFactory для NavManager

---
+++ module/Application/src/Service/Factory/NavManagerFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Application\Service\Factory;
+
+use Application\Service\NavManager;
+use Laminas\Authentication\AuthenticationService;
+use Psr\Container\ContainerInterface;
+use User\Service\RbacManager;
+
+class NavManagerFactory
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $authService = $container->get(AuthenticationService::class);
+
+        $viewHelperManager = $container->get('ViewHelperManager');
+        $urlHelper = $viewHelperManager->get('url');
+        $rbacManager = $container->get(RbacManager::class);
+
+        return new NavManager($authService, $urlHelper, $rbacManager);
+    }
+}
