c4a064d0a20f795eaab386275e35e8f16aa1fb1b
Roma Volkov <Drakyla60@gmail.com>
2021-11-03 15:34:42 +0200

    Переписав generatePasswordResetToken надсилання листа через сервіс mailManager->sendMail

--- module/User/src/Service/UserManager.php
+++ module/User/src/Service/UserManager.php
@@ -238,32 +238,14 @@ class UserManager
             'user/email/reset-password-email',
             ['passwordResetUrl' => $passwordResetUrl,]);
 
-        //@TODO Винести то колись звідси
-        $mail = new PHPMailer(true);
 
-        try {
-            //Server settings
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'smmithadam@gmail.com';                     //SMTP username
-            $mail->Password   = 'favorite_world';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $option = [
+            'subjectEmail' => 'Скидання пароля.',
+            'bodyHtml' => $bodyHtml,
+        ];
 
-            //Recipients
-            $mail->setFrom('admin@example.com', 'Administration');
-            $mail->addAddress($user->getEmail(), $user->getFullName());     //Add a recipient
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Reset Password !!! ';
-            $mail->Body    = $bodyHtml;
-
-            $mail->send();
-            echo '<p> Лист надіслано. За декілька хвилин перевірте свою  адресу <b> ' .  $user->getEmail() . '</b></p>';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        if ( true == $this->mailManager->sendMail($user, $option)) {
+            echo 'Лист для скидання пароля надіслано, превірте пошту : ' . $user->getEmail();
         }
 
     }
