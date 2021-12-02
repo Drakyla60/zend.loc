b699a8b20041b9caee382064d669b57bd8f5d437
Roma Volkov <Drakyla60@gmail.com>
2021-12-02 19:36:54 +0200

    Сутність для роботи з постами в mongoDB

---
+++ module/Admin/src/Entity/Post.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Admin\Entity;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * @ODM\Document(collection="post")
+ *
+ */
+class Post
+{
+    /** @ODM\Id */
+    protected $id;
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $author;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $title;
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $description;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $dateCreated;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $tags;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $content;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param mixed $dateCreated
+     */
+    public function setDateCreated($dateCreated): void
+    {
+        $this->dateCreated = $dateCreated;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param mixed $tags
+     */
+    public function setTags($tags): void
+    {
+        $this->tags = $tags;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * @param mixed $rating
+     */
+    public function setRating($rating): void
+    {
+        $this->rating = $rating;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getViews()
+    {
+        return $this->views;
+    }
+
+    /**
+     * @param mixed $views
+     */
+    public function setViews($views): void
+    {
+        $this->views = $views;
+    }
+
+    /**
+     * @ODM\Field(type="integer")
+     */
+    protected $rating;
+
+    /**
+     * @ODM\Field(type="integer")
+     */
+    protected $views;
+
+}
