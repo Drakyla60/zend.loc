623048743d2dda1ff5f681fb13adb2d7f434b0d5
Roma Volkov <Drakyla60@gmail.com>
2021-12-02 19:38:46 +0200

    Написав Parser для парсингу записів з блогу

---
+++ module/Admin/src/Service/Parser/Parser.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Admin\Service\Parser;
+use Admin\Service\ParseInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Parser implements ParseInterface
+{
+
+    const PARSE_URL = 'https://laravel.ru/posts';
+
+    public function parse()
+    {
+        $pageParseUrl = [ self::PARSE_URL ];
+
+        for ($i = 1;$i <= 35 ;$i++){
+            $getPages[] = 'https://laravel.ru/posts?page=' . $i;
+            $pageParseUrl = array_merge( $getPages);
+        }
+
+        $getPostsUrl = [];
+
+        foreach ($pageParseUrl as $item) {
+            $normalizeUrl = $this->createNormalizeUrl($item);
+
+            $getContent = $this->getHtml($normalizeUrl);
+
+            $crawler = new Crawler($getContent);
+
+            $getPages = $crawler->filter('h2.hvl-h2 > a')
+                ->each(function (Crawler $link) {
+                    return $link->attr('href');
+                });
+            $getPostsUrl = array_merge($getPostsUrl, $getPages);
+
+            if (count($getPostsUrl) == 100) {
+                break;
+            }
+        }
+
+
+        $array = [];
+        foreach ($getPostsUrl as $item) {
+            $getContentTopic = $this->getHtml($item);
+            $crawlerTopic = new Crawler($getContentTopic);
+
+            $post = $crawlerTopic
+                ->filter('div.hvl-content')
+                ->each(function (Crawler $crawler) {
+
+                    $tagsCount = $crawler->filter('p.hvl-tags > a')->count();
+                    $tags = [];
+                    if (0 < $tagsCount) {
+                        for ($i = 1; $i <= $tagsCount; $i++) {
+                            $tags[] = $crawler->filter('p.hvl-tags > a:nth-of-type('. $i .')')->text();
+                            $tags = array_merge($tags);
+                        }
+                    }
+                    return  [
+                        'postTitle' => $crawler->filter('h1.hvl-h1')->text(),
+                        'postAuthor' => $crawler->filter('p.hvl-post-author > span > a > i')->text(),
+                        'postDateCreated' => $crawler->filter('p.hvl-post-author > time')->attr('datetime'),
+                        'postTags' => $tags,
+                        'postContent' => $crawler->filter('article.hvl-markedup')->text(),
+                        'postRating' => $crawler->filter('span.hvl-post-footer-ctl > b')->text(),
+                        'postViews' => $crawler->filter('span.hvl-post-footer-ctl:nth-of-type(2)')->text(),
+                    ];
+                });
+
+            $array = array_merge($array, $post);
+        }
+
+        return $array;
+    }
+
+
+    /**
+     * Нормалізація адреси $url
+     * @param $baseUrl
+     * @return string
+     */
+    public function createNormalizeUrl($baseUrl)
+    {
+        return  ltrim($baseUrl, './');
+    }
+
+    /**
+     * Парсинг сторінки за $url
+     * @param $url
+     * @return false|string
+     */
+    public function getHtml($url)
+    {
+        return file_get_contents($url);
+    }
+
+    //Паралельний парсинг
+    public function parallel_map(callable $func, array $items)
+    {
+        $childPids = [];
+        $result = [];
+        foreach ($items as $i => $item) {
+            $newPid = pcntl_fork();
+            if ($newPid == -1) {
+                die('Can\'t fork process');
+            } elseif ($newPid) {
+                $childPids[] = $newPid;
+                if ($i == count($items) - 1) {
+
+                    foreach ($childPids as $childPid) {
+                        pcntl_waitpid($childPid, $status);
+                        $sharedId = shmop_open($childPid, 'a', 0, 0);
+                        $shareData = shmop_read($sharedId, 0, shmop_size($sharedId));
+                        $result[] = unserialize($shareData);
+                        shmop_delete($sharedId);
+                        shmop_close($sharedId);
+                    }
+                }
+            } else {
+                $myPid = getmypid();
+                echo 'Start ' . $myPid . PHP_EOL;
+                $funcResult = $func($item);
+                $shareData = serialize($funcResult);
+                $sharedId = shmop_open($myPid, 'c', 0644, strlen($shareData));
+                shmop_write($sharedId, $shareData, 0);
+                echo 'Done ' . $myPid . ' ' . memory_get_peak_usage() . PHP_EOL;
+//            exit(0);
+                posix_kill(getmypid(), SIGKILL);
+            }
+        }
+        return $result;
+    }
+}
