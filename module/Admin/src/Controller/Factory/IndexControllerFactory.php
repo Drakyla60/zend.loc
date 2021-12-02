d4fc314291265b6dd52de9cccea6037bdcc774ac
Roma Volkov <Drakyla60@gmail.com>
2021-12-02 19:40:10 +0200

    Додав нові залежності IndexControllerFactory

--- module/Admin/src/Controller/Factory/IndexControllerFactory.php
+++ module/Admin/src/Controller/Factory/IndexControllerFactory.php
@@ -3,15 +3,16 @@
 namespace Admin\Controller\Factory;
 
 use Admin\Controller\IndexController;
+use Admin\Service\Parser\Parser;
 use Interop\Container\ContainerInterface;
 
 class IndexControllerFactory
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): IndexController
     {
-        $mongoManager = $container->get('doctrine.documentmanager.odm_default');
-
+        $mongoManager     = $container->get('doctrine.documentmanager.odm_default');
+        $parser           = $container->get(Parser::class);
         $sessionContainer = $container->get('ContainerNamespace');
-        return new IndexController($sessionContainer, $mongoManager);
+        return new IndexController($sessionContainer, $mongoManager, $parser);
     }
 }
